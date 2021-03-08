248f215abffbf10441f6f24f031606d0cb21896b
Sai sirisha maddula <saisirisham@iquadra.com>
2021-03-07 23:11:10 -0500

    Charts added with sample Data

--- src/components/NowWhat.tsx
+++ src/components/NowWhat.tsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from './CardHeader';
-import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
-import Avatar from './Avatar';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 const useStyles = makeStyles({
   card: {
@@ -22,9 +18,55 @@ export default () => {
   const [heartBeat, setHeartBeat] = useState(0);
   const requestData = {"query":" query{  heartBeat }"}
   const classes = useStyles();
+  let data: { name: string, uv: number}[] = [];
+  const [chart, setChartData] = useState(data);
+ const sampleData = [
+  {
+    name: 'HeartBeat 1',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'HeartBeat 2',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'HeartBeat 3',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'HeartBeat 4',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'HeartBeat 5',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'HeartBeat 6',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'HeartBeat 7',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
   useEffect(() => {
-  fetch('https://react.eogresources.com/graphql', {
+    const interval =  setInterval(() => fetch('https://react.eogresources.com/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -32,14 +74,18 @@ export default () => {
     body: JSON.stringify(requestData),
   })
   .then(response => response.json())
-  .then(data => {
-    console.log('Success:',data.data.heartBeat, data);
-    setHeartBeat(data.data.heartBeat);
+  .then(resp => {
+    console.log('Success:',resp.data);
+    data.push({ name: 'heartBeat', uv: resp.data.heartBeat });
+   setChartData(data);
+    setHeartBeat(resp.data.heartBeat);
   })
   .catch((error) => {
     console.error('Error:', error);
-  });
+  }), 1300);
+  return () => clearInterval(interval);
 },[]);
+console.log('chart', chart);
   return (
     <Card className={classes.card}>
       <CardHeader title="OK, sai sirisha, you're all setup. Now What?" />
@@ -48,41 +94,22 @@ export default () => {
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-         
-          // onChange={handleChange}
         >
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
+        {chart}
         {heartBeat}
-        <List>
-          <ListItem>
-            <Avatar>1</Avatar>
-            <ListItemText primary="Explore the GraphQL API" />
-          </ListItem>
-          <ListItem>
-            <Avatar>2</Avatar>
-            <ListItemText primary="Add ability to select Metrics" />
-          </ListItem>
-          <ListItem>
-            <Avatar>3</Avatar>
-            <ListItemText primary="Display the current metric data" />
-          </ListItem>
-          <ListItem>
-            <Avatar>4</Avatar>
-            <ListItemText primary="Chart historical metric data" />
-          </ListItem>
-          <ListItem>
-            <Avatar>5</Avatar>
-            <ListItemText primary="Submit Your App" />
-          </ListItem>
-        </List>
-
-        <Typography variant="body1">
-          Remember to refer to our <a href="https://react.eogresources.com/assessing">How We Assess Submissions</a>{' '}
-          guidelines, as well as the <a href="https://react.eogresources.com/api">GraphQL API Documentation</a>.
-        </Typography>
+        {/* <LineChart width={500} height={300} data={chart}> */}
+        <LineChart width={500} height={300} data={sampleData}>
+    <XAxis dataKey="name"/>
+    <YAxis/>
+    <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+  </LineChart>
+     
       </CardContent>
     </Card>
   );
